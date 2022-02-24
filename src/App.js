9ab5ac22bf637eeac33bdb325b6834cbf78c9f6f
ToeWaiPhyo <toewaiphyo@ucstgi.edu.mm>
2022-02-24 11:30:25 +0630

    making separate components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
 import React from 'react';
 import classes from './App.module.css';
-import ProductData from './ProductData';
+import ProductDetail from './ProductDetail';
+import ProductPreview from './ProductPreview';
+import TopBar from './TopBar';
 
 function App() {
-  const currentHour=new Date().getHours()>9?new Date().getHours():'0'+new Date().getHours();
-  const currentminute=new Date().getMinutes()>9?new Date().getMinutes():'0'+new Date().getMinutes();
+  
   return (
     <div className="App">
-      <header className='App-header'>
-        <nav className={classes.Topbar}>
-          <img src='https://i.dlpng.com/static/png/197684_preview.png' alt='Amazon Logo'/>
-        </nav>
-      </header>
+        <TopBar/>
+      
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
-          <img src='https://imgur.com/iOeUBV7.png' alt='Product Preview'/>
-          {/* <div className={classes.TimeSection}>
-            <p>{`${currentHour}:${currentminute}`}</p>
-          </div> */}
-          <div className={classes.Heartbeat}>
-            <i class="fas fa-heartbeat"></i>
-            <p>78</p>
-          </div>
+        
+          <ProductPreview/>
         </div>
         <div className={classes.ProductData}>
-        <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-        <p className={classes.ProductDescription}>{ProductData.description}</p>
-        <h3 className={classes.SectionHeading}>Select Color</h3>
-        <div>
-          <img className={[classes.ProductImage,classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt='Black Colored Watch'/>
-          <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt='Red Colored Watch'/>
-          <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt='Blue Colored Watch'/>
-          <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt='Purple Colored Watch'/>
-        </div>
-        <h3 className={classes.SectionHeading}>Features</h3>
-        <div>
-          <button className={[classes.FeatureItem,classes.SelectedFeatureItem].join(' ')}>Time</button>
-          <button className={classes.FeatureItem}>Heart Rate</button>
-        </div>
-        <button className={classes.PrimaryButton}>Buy Now</button>
+        <ProductDetail/>
         </div>
         
       </div>
